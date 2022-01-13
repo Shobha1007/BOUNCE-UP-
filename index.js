8406860877f29128d4633c55d136b0b52ae4e0bd
Shobha <shobhasinghncr@gmail.com>
2022-01-13 21:48:04 +0530

    added functions 1. moveplat,2.jump ,3. fall , 4.gameover.

--- index.js
+++ index.js
@@ -5,38 +5,88 @@ document.addEventListener('DOMContentLoaded', () => {
     const gridheight = 600;
     let platCount = 5;
     const platGap = gridheight / platCount;
-
+    let platforms = [];
+    let playerbottom=250;
+    let playerleft=50;
+    let isJumping=false;
+    
+    
     function createPlayer() {
         grid.appendChild(player);
         player.classList.add('player');
+        playerbottom = platforms[0].bottom + 15;
+        playerleft = platforms[0].left;
+        player.style.bottom=playerbottom + 'px';
+        player.style.left = playerleft + 'px';
     }
 
     class Platform {
         constructor(newPlatBottom){
         this.bottom = newPlatBottom;
         this.left = Math.random() * 300;
+        let platform = document.createElement("div");
+        this.visual=platform;
         }
 
         draw(){
-            let platform = document.createElement("div");
-            grid.appendChild(platform);
-            platform.classList.add('platform');
-            platform.style.left=this.left + 'px';
-            platform.style.bottom=this.bottom + 'px';
+            
+            grid.appendChild(this.visual);
+            this.visual.classList.add('platform');
+            this.visual.style.left=this.left + 'px';
+            this.visual.style.bottom=this.bottom + 'px';
         }
     }
     function createPlatform(){
         for (let i=0;i<platCount ; i++){
             let newPlatBottom = 100 + platGap*i;
             let newPlat=new Platform(newPlatBottom);
+            platforms.push(newPlat);
             newPlat.draw();
         }
     }
+    function movePlat(){
+        if (playerbottom>200)
+        platforms.forEach(p =>{
+            p.bottom-=4;
+            let platform = p.visual;
+            platform.style.bottom= p.bottom + 'px';
+        })
+    }
+    function jump(){
+        // clearInterval(downward);
+        isJumping=true;
+        upward = setInterval(function(){
+            playerbottom+=20;
+            player.style.bottom=playerbottom + 'px';
+            if (playerbottom > 350){
+                fall();
+            }
 
+        },30)
+    }
+    function fall(){
+        isJumping=false;
+        clearInterval(upward);
+        downward = setInterval(function(){
+            playerbottom-=5;
+            player.style.bottom=playerbottom + 'px';
+            if (playerbottom <=0){
+                gameOver();
+            }
+
+        },30)
+    }
+    function gameOver(){
+        GameOver=true;
+        clearInterval(upward);
+        clearInterval(downward);
+    }
     function start() {
         if (!GameOver) {
-            createPlayer();
             createPlatform();
+            createPlayer();
+            setInterval(movePlat,30);
+            jump();
         }
     }
     start();
